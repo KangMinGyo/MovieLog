1a46a2de92a0ffd8551beec6eb6ea7b00be165ed
KangMinGyo <kang15567@kakao.com>
2023-12-19 23:23:53 +0900

    feat: HomeViewController추가

--- MovieLog/Home/HomeViewController.swift
+++ MovieLog/Home/HomeViewController.swift
@@ -8,25 +8,25 @@
 import UIKit
 import SnapKit
 import Then
+import Combine
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct MyHomeViewControllerPreview: PreviewProvider {
+    static var previews: some View {
+        ContentViewControllerPreview {
+            let vc = HomeViewController()
+            return vc
+        }
+    }
+}
+#endif
