97e1842dc8c26576b06ca87e76e9126511d1673e
KangMinGyo <kang15567@kakao.com>
2024-02-01 04:47:31 +0900

    feat: Charts 라이브러리 추가

--- MovieLog/View/Calendar/CalendarCell.swift
+++ MovieLog/View/Calendar/CalendarCell.swift
@@ -24,6 +24,14 @@ class CalendarCell: UICollectionViewCell {
         $0.numberOfLines = 0
     }
     
+    let directorNameLabel = UILabel().then {
+        $0.text = "영화 감독"
+    }
+    
+    let movieInfoLabel = UILabel().then {
+        $0.text = "미국 | 미스터리, 범죄 | 2000"
+    }
+    
     let dateLabel = UILabel().then {
         $0.text = "작성일: 23.01.19 오후 5:23"
     }
@@ -54,21 +62,31 @@ class CalendarCell: UICollectionViewCell {
     private func setupConstraints() {
         addSubview(posterImageView)
         addSubview(movieNameLabel)
+        addSubview(directorNameLabel)
+        addSubview(movieInfoLabel)
         addSubview(dateLabel)
         
         posterImageView.snp.makeConstraints {
             $0.top.leading.bottom.equalTo(0).inset(20)
-            $0.width.equalTo(40)
+            $0.width.equalTo(75)
         }
         
         movieNameLabel.snp.makeConstraints {
-            $0.top.equalTo(posterImageView.snp.top)
-            $0.trailing.equalTo(0).inset(20)
+            $0.top.trailing.equalTo(0).inset(20)
+            $0.leading.equalTo(posterImageView.snp.trailing).offset(20)
+        }
+        
+        directorNameLabel.snp.makeConstraints {
+            $0.top.equalTo(movieNameLabel.snp.bottom).offset(5)
+            $0.leading.equalTo(posterImageView.snp.trailing).offset(20)
+        }
+        
+        movieInfoLabel.snp.makeConstraints {
+            $0.top.equalTo(directorNameLabel.snp.bottom).offset(5)
             $0.leading.equalTo(posterImageView.snp.trailing).offset(20)
         }
         
         dateLabel.snp.makeConstraints {
-            $0.top.equalTo(movieNameLabel.snp.bottom).offset(5)
             $0.leading.equalTo(posterImageView.snp.trailing).offset(20)
             $0.bottom.equalTo(self.snp.bottom).offset(-20)
         }
@@ -89,9 +107,13 @@ class CalendarCell: UICollectionViewCell {
     
     func setup(with data: Review) {
         posterImageView.image = UIImage(named: "Poster")
+        print("Data: \(data)")
+        print("Title: \(data.uid), PosterURL: \(data.posterURL)")
         let url = URL(string: "https://image.tmdb.org/t/p/original\(data.posterURL)")
         posterImageView.kf.setImage(with: url)
         movieNameLabel.text = data.title
+        directorNameLabel.text = data.director
+        movieInfoLabel.text = data.movieInfo
         if let date = data.date {
             let dateFormatterManager = DateFormatterManager()
             let dateString = dateFormatterManager.dateFormatter.string(from: date)
