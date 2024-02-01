061b4fa12971df621901b67fb122cc94d11ad143
KangMinGyo <kang15567@kakao.com>
2024-02-01 18:33:16 +0900

    feat: BarChartView 추가

--- MovieLog/View/Tab/TabViewController.swift
+++ MovieLog/View/Tab/TabViewController.swift
@@ -17,14 +17,19 @@ final class TabViewController: UITabBarController {
         let tab2 = CalendarViewController()
         tab2.title = "캘린더"
         
+        let tab3 = ChartViewController()
+        tab2.title = "차트"
+        
         let nav1 = UINavigationController(rootViewController: tab1)
         let nav2 = UINavigationController(rootViewController: tab2)
+        let nav3 = UINavigationController(rootViewController: tab3)
         
         nav1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
         nav2.tabBarItem = UITabBarItem(title: "Calendar", image: UIImage(systemName: "calendar"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Chart", image: UIImage(systemName: "chart.bar"), tag: 3)
         
         setViewControllers([
-            nav1, nav2,
+            nav1, nav2, nav3
         ], animated: true)
     }
 }
--- MovieLog/View/chart/ChartViewController.swift
+++ MovieLog/View/chart/ChartViewController.swift
@@ -6,24 +6,37 @@
 //
 
 import UIKit
+import Charts
+import DGCharts
 
 class ChartViewController: UIViewController {
 
+    // MARK: - UI Components
+    var barChartView: BarChartView = BarChartView()
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        setupConstraints()
+        setupBarChart()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - UI Setup
+    func setupConstraints() {
+        view.addSubview(barChartView)
+        
+        barChartView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaInsets).inset(20)
+            $0.leading.trailing.equalTo(view.safeAreaInsets).inset(20)
+            $0.bottom.equalTo(view.safeAreaInsets)
+        }
     }
-    */
-
+    
+    func setupBarChart() {
+        self.barChartView.noDataText = "출력 데이터가 없습니다."
+        self.barChartView.noDataFont = .systemFont(ofSize: 20)
+        self.barChartView.noDataTextColor = .lightGray
+        self.barChartView.backgroundColor = .white
+    }
 }
