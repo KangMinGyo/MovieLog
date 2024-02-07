84a20649c9d61f4c110c08ff7f4f0ab99e995bb8
KangMinGyo <kang15567@kakao.com>
2024-02-07 23:09:00 +0900

    feat: SettingViewController 추가

---
+++ MovieLog/View/setting/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  MovieLog
+//
+//  Created by KangMingyo on 2/7/24.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
