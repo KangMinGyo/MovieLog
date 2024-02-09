b0f23e97290d7e4744e4cd365c76bb00893ef81a
KangMinGyo <kang15567@kakao.com>
2024-02-09 15:26:16 +0900

    feat: Cell 간격 조정

--- MovieLog/View/Home/HomeViewController.swift
+++ MovieLog/View/Home/HomeViewController.swift
@@ -26,7 +26,6 @@ class HomeViewController: UIViewController {
     lazy var collectionView: UICollectionView = {
         let flowlayout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowlayout)
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
         return collectionView
     }()
     
@@ -95,6 +94,9 @@ class HomeViewController: UIViewController {
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
         
         let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        section.interGroupSpacing = 10
+        
         let layout = UICollectionViewCompositionalLayout(section: section)
         return layout
     }
@@ -127,11 +129,39 @@ class HomeViewController: UIViewController {
 }
 
 //extension HomeViewController: UICollectionViewDataSource {
+//    
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return viewModel.reviews.count
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCell.identifier, for: indexPath) as! HomeCell
+//        cell.setup(with: viewModel.reviews[indexPath.row])
+//        return cell
+//    }
+//    
 ////    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 ////        let nextVC = MovieReviewDetailViewController()
 ////        nextVC.viewModel.movieData = viewModel.review[indexPath.row]
 ////        self.show(nextVC, sender: self)
 ////    }
+//}
+
+//extension HomeViewController: UICollectionViewDelegateFlowLayout {
+//    //Cell의 크기
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: view.frame.width - 20, height: view.frame.width / 2.5)
+//    }
+//    
+//    //Cell간의 간격
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 10
+//    }
+//    
+//    //상하좌우 공백
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+//    }
 //}
 
 #if canImport(SwiftUI) && DEBUG
